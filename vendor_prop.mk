908904af87de8cb9eb9e7e41dc326f156b1e833c
Linvn <llin1079@gmail.com>
2020-05-14 10:53:34 +0800

    Enable screen navbar and set DPI=420

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -18,4 +18,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.qfp=false \
     ro.hardware.fingerprint=fpc \
-    sys.fpc.tu.disabled=0
+    sys.fpc.tu.disabled=0 \
+	qemu.hw.mainkeys=0 \
+    ro.sf.lcd_density=420
